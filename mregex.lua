0695346f43f363591be09c1e5dde8d6f89e16f65
MorganPeterson <lastyearsmodel@gmail.com>
2021-05-27 21:32:42 -0400

    added first flags and options

--- mregex.lua
+++ mregex.lua
@@ -1,10 +1,73 @@
-local xflag = true
-local wflag = true
-local eflag = true
+local Aflag = 0
+local Bflag = 0
+local bflag = false
+local xflag = false
+local wflag = false
+local eflag = false
 local fflag = false
+local gflag = true
 local matchall = true
 
+local optstr= "0123456789A:B:C:EFGHILPSRUVZabce:f:hilnoqrsuvwxy"
 local pattern = ""
+local opt_table = {}
+
+function getopt(optstring, ...)
+    local opts = { }
+    local args = { ... }
+
+    for optc, optv in optstring:gmatch"(%a)(:?)" do
+        opts[optc] = { hasarg = optv == ":" }
+    end
+
+    return coroutine.wrap(function()
+        local yield = coroutine.yield
+        local i = 1
+
+        while i <= #args do
+            local arg = args[i]
+
+            i = i + 1
+
+            if arg == "--" then
+                break
+            elseif arg:sub(1, 1) == "-" then
+                for j = 2, #arg do
+                    local opt = arg:sub(j, j)
+
+                    if opts[opt] then
+                        if opts[opt].hasarg then
+                            if j == #arg then
+                                if args[i] then
+                                    yield(opt, args[i])
+                                    i = i + 1
+                                elseif optstring:sub(1, 1) == ":" then
+                                    yield(':', opt)
+                                else
+                                    yield('?', opt)
+                                end
+                            else
+                                yield(opt, arg:sub(j + 1))
+                            end
+
+                            break
+                        else
+                            yield(opt, false)
+                        end
+                    else
+                        yield('?', opt)
+                    end
+                end
+            else
+                yield(false, arg)
+            end
+        end
+
+        for i = i, #args do
+            yield(false, args[i])
+        end
+    end)
+end
 
 local function add_pattern(pat, len)
     -- match all or die trying
@@ -72,6 +135,78 @@ local function read_patterns(file_name)
     end
     f:close()
 end
+
+local lastopt = nil
+local newarg = nil
+local optindex = 1
+local prevoptindex = 0
+local need_pattern = true
+
+for opt, arg in getopt(optstr, ...) do
+    local opt_num = tonumber(opt)
+    if opt_num and opt_num > -1 and opt_num < 10 then
+        if newarg or tonumber(lastc) == nil then
+            Aflag = 0
+        end
+        Bflag = (Aflag * 10) + opt_num
+        Aflag = Bflag
+    elseif opt == 'A' or opt == 'B' then
+        local arg_num = tonumber(arg)
+        if not arg_num or arg_num <= 0 then
+            print(2, "context out of range")
+            return
+        end
+        if opt == 'A' then
+            Aflag = arg_num
+        else
+            Bflag = arg_num
+        end
+    elseif opt == 'b' then
+        bflag = true
+    elseif opt == 'C' then
+        if not arg then
+            Aflag = 2
+            Bflag = 2
+        else
+            local arg_num = tonumber(arg)
+            if not arg_num or arg_num <= 0 then
+                print(2, "context out of range")
+                return
+            end
+            Aflag = arg_num
+            Bflag = arg_num
+        end
+    elseif opt == 'e' then
+        add_patterns(arg)
+        need_pattern = false
+    elseif opt == 'E' then
+        eflag = true
+        fflag = false
+        gflag = false
+    elseif opt == 'f' then
+        read_patterns(arg)
+        need_pattern = false
+    elseif opt == 'F' then
+        eflag = false
+        fflag = true
+        gflag = false
+    elseif opt == 'F' then
+        eflag = false
+        fflag = false
+        gflag = true
+    elseif opt == 'w' then
+        wflag = true
+    elseif opt == 'x' then
+        xflag = true
+    else
+        usage()
+    end
+    lastopt = opt
+    newarg = optindex ~= prevoptindex
+    prevoptindex = optindex
+    optindex = optindex + 1
+end
+
 
 local test = "^ab?c\\d.*ef^go\td\nabcd?ef$\n"
 add_patterns(test)
